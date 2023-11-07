310e4a3c1b381fad69407f12b60d8419543682ae
EmelKrist <kristina_1516@mail.ru>
2023-11-07 18:54:58 +0800

    project init

---
+++ src/test/java/ru/emelkrist/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.emelkrist.librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
